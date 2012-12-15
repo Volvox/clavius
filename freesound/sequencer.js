b2328a138e17c1e0773c8e2e5b11e9db9afa4a94
Volvox <m2455314@gmail.com>
2012-12-14 20:59:51 -0500

    freesound folder

---
+++ freesound/sequencer.js
@@ -0,0 +1,15 @@
+// var Sequencer = {
+//     BPM : 120,
+//     ROWS : '',
+//     COLS : 8,
+//     _make_grid : function(){
+//        for(var r=0; r<ROWS; ++r){
+//             for(var c=0; c<COLS; ++c){
+
+//             }
+//        }
+//     }
+
+// };
+
+
